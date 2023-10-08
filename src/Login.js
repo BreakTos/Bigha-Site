b209ffb7cff6fd495d53c7f6241c11f9bd15a2a5
Parth Seth <parthseth2004@gmail.com>
2023-10-09 01:26:41 +0530

    store data in state

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,41 @@
 import './Login.css'
+import React,{useState} from 'react';
+
+
+let name,value;
+
 
 function Login(){
+    const [userData,setUserData] = useState({
+        userNumber: "",
+        userPass: "",
+    });
+    
+    function postUserData(event){
+        name=event.target.name;
+        value=event.target.value;
+    
+        setUserData({...userData,[name]:value});
+    }
+    
+    function submitData(event){
+        event.preventDefault();
+        
+        
+    }
+
     return(
         <>
         <div className='loginBox'>
             <center>
             <h4>Login to Bigha</h4>
             </center>
-            <form className='form'>
+            <form className='form' method='POST'>
                 <p>&nbsp;&nbsp;Enter mobile number</p>
-                <input type='mobile'/>
+                <input type='mobile' name='userNumber' value={userData.userNumber} onChange={postUserData}/>
                 <p>&nbsp;&nbsp;Enter Password</p>
-                <input type='password'/> 
-                <input style={{margin:'10% 30%' , backgroundColor:'beige' }} type='submit'/>
+                <input type='password'name='userPass' value={userData.userPass} onChange={postUserData}/> 
+                <input style={{margin:'10% 30%' , backgroundColor:'beige' }} type='submit' onClick={submitData}/>
             </form>
         </div>
         </>
